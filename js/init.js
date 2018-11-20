21f5b6822849d3b4c2d0f2b47ca1ae4c714e7737
Willis Wang <wwang4292@gmail.com>
2018-11-20 13:28:26 -0800

    first commit

---
+++ js/init.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.collapsible').collapsible({
+        accordion: false
+    });
+});
+(function($){
+  $(function(){
+    
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+  
+})(jQuery); // end of jQuery name space
